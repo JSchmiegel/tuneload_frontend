10a09815eb88541630b09672130ea5033695d02a
JSchmiegel <jonas@ullr>
2021-12-11 11:31:48 +0100

    removed unused code snippets

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:collection';
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spotiload/backen_communicator.dart';
 import 'package:spotiload/global_var.dart';
-import 'package:http/http.dart' as http;
 import 'package:spotiload/settings.dart';
 
 class SettingScreen extends StatefulWidget {
@@ -87,56 +85,15 @@ class _SettingScreenState extends State<SettingScreen> {
                   }
                   return const CircularProgressIndicator();
                 })));
-    // getProperWidget()
-    // ])));
   }
 
   Future<Settings> _callBackendApi() async {
     var api = BackendCommunicator();
-    final response = await api.getSettings()
-        // .then((response) {
-        //   setState(() {
-        //     apiCall = false; //Disable Progressbar
-        //     // _currentSettings = weather.toString();
-        //     _currentSettings = response.body;
-        //   });
-        // }, onError: (error) {
-        //   setState(() {
-        //     apiCall = false; //Disable Progressbar
-        //     _currentSettings = error.toString();
-        //   });
-        ;
+    final response = await api.getSettings();
     if (response.statusCode == 200) {
       return Settings.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load settings');
     }
-
-    // print('Response status: ${response.statusCode}');
-    // if (response.statusCode == 200) {
-    //   apiCall = false;
-    // } else {
-    //   apiCall = false;
-    //   _currentSettings = "ERROR!!";
-    // }
   }
-
-  // [ ] probably better with async or await?!!
-  // Widget getProperWidget() {
-  //   if (apiCall) {
-  //     return CircularProgressIndicator();
-  //   } else {
-  //     var _currentSettingsJson = jsonDecode(_currentSettings);
-  //     try {
-  //       return Text(
-  //         _currentSettingsJson['paths'],
-  //       );
-  //     } catch (e) {
-  //       return const Text('There is an error with the answer from the server');
-  //     }
-  //   }
-  // }
-
-  // String _currentSettings = "{}";
-  // bool apiCall = false;
 }
